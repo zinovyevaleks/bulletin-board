52f51504aed7003627cf2979d2012470189776c8
Александр <zinovyevaleks@yandex.ru>
2021-11-29 15:44:36 +0700

    initial project setup and creation of a navigation block

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-tile v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="amber lighten-1">
+      <v-toolbar-side-icon 
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="link in links" :key="link.title" :to="link.url">
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "lock", url: "/login" },
+        { title: "Registration", icon: "face", url: "/registration" },
+        { title: "Orders", icon: "bookmark_border", url: "/orders" },
+        { title: "New", icon: "note_add", url: "/new" },
+        { title: "My ads", icon: "list", url: "/list" },
+        ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
